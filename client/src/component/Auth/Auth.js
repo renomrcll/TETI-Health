9f3d5e6e857171aa342556f3bf500a2f7b5953c0
reno marcello <renom4@gmail.com>
2022-07-08 06:49:04 +0700

    add img wrapper

--- client/src/component/Auth/Auth.js
+++ client/src/component/Auth/Auth.js
@@ -56,6 +56,7 @@ export default class Auth extends React.Component {
         }
         );
     };
+
     changeTab = () => {
         this.setState({
             tab : this.state.tab === "signup" ? "signin" : "signup"
@@ -67,6 +68,9 @@ export default class Auth extends React.Component {
     return (
         <div className="auth-wrapper">
             <div className="left-wrapper">
+                <div className="img-wrapper">
+                    <img src="" alt="logo"/>
+                </div>
 
             </div>
             <div className="right-wrapper">
